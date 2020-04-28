1f9f3a5decc059892d6e5b3dff79454821f3b47b
Benjamin Thom <jaminthom@gmail.com>
2020-04-28 11:59:40 -0700

    update src/__tests__/reducers/post-list-reducer.test.js and src/reducers/post-list-reducer.js to successfully fail and pass 2nd test
    Co-Authored-By: Alex Skreen <skreenalex@gmail.com>
    o-authored-by: Jamison Cozart <jamisoncozart@gmail.com>

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -1,7 +1,53 @@
 import postListReducer from '../../reducers/post-list-reducer';
 
 describe('postListReducer', () => {
+  let action;
+  const currentState = {
+    1: { user: 'Test User',
+      title: 'Test Title',
+      body: 'Test Body',
+      upvotes: 'Test upvotes',
+      id: 1 
+    },
+    2: { user: 'Test User2',
+      title: 'Test Title2',
+      body: 'Test Body2',
+      upvotes: 'Test upvotes2',
+      id: 2 
+    }
+  }
+  
+  const postData = {
+    user: 'Test User',
+    title: 'Test Title',
+    body: 'Test Body',
+    upvotes: 'Test upvotes',
+    id: 1
+  };
+  
   test('Should return default state if there is no action type passed into the reducer', () => {
-    expect(postListReducer({}, { type: null })).toEqual({});
+    expect(postListReducer({}, { type: null })).toEqual({})
+  });
+
+  test('Should successfully add new post data to masterPostList', () => {
+    const { user, title, body, upvotes, id } = postData;
+      action = {
+        type: 'ADD_POST',
+        user: user,
+        title: title,
+        body: body,
+        upvotes: upvotes,
+        id: 1
+      };
+
+    expect(postListReducer({}, action)).toEqual({
+      [id]: {
+        user: user,
+        title: title,
+        body: body,
+        upvotes: upvotes,
+        id: id
+      }
+    });
   });
 });
--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,3 +1,22 @@
 export default (state = {}, action) => {
-  return state;
+  const { user, title, body, upvotes, id } = action;
+  switch (action.type) {
+  case 'ADD_POST':
+    return Object.assign({}, state, {
+      [id]: {
+        user: user,
+        title: title,
+        body: body,
+        upvotes: upvotes,
+        id: id
+      }
+    });
+  case 'DELETE_POST':
+    const newState = { ...state };
+    delete newState[id];
+    return newState;
+
+  default:
+    return state;
+  }
 };
