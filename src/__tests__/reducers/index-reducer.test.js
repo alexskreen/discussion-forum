be45c59c3bda08dfae9a74265b15076be1121f3b
Benjamin Thom <jaminthom@gmail.com>
2020-04-28 14:36:25 -0700

    update src/reducers/index-reducer.js and src/__tests__/reducers/index-reducer.test.js to successfully fail and pass remianing tests
    Co-Authored-By: Alex Skreen <skreenalex@gmail.com>
    o-authored-by: Jamison Cozart <jamisoncozart@gmail.com>

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -18,4 +18,29 @@ describe('rootReducer', () => {
     expect(store.getState().masterPostList).toEqual(postListReducer(undefined, { type: null }));
   });
 
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of postListReducer matches root reducer', () => {
+    const action = {
+      type: 'ADD_POST',
+      user: 'Test User',
+      title: 'Test Title',
+      body: 'Test Body',
+      upvotes: 'Test Upvotes',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterPostList).toEqual(postListReducer(undefined, action));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
 });
